6ef2a4693bdba91453f22058487c024954c645af
Daniel Papilion <papiliond.94@gmail.com>
2022-03-11 16:57:14 +0100

    Crawled urls for authors/* pages

---
+++ pages/authors/_author.vue
@@ -0,0 +1,52 @@
+<template>
+  <main class="pa3">
+    <h1>Author: {{ author.name }}</h1>
+    <nav>
+      <button @click="handleQuotesButtonClicked">Give me some quotes</button>
+    </nav>
+    <div class="pa3" v-html="rawHtml"></div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const { data: author } = await $axios.get(
+      'https://programming-quotes-api.herokuapp.com/Authors/' +
+        encodeURIComponent(params.author)
+    )
+
+    if (!author.quotes || !author.quotes.length) {
+      throw {
+        statusCode: 404,
+        message: `Sorry, We don't have any data about the author "${params.author}".`,
+      }
+    }
+    const wikiPage = author.wikiUrl.split('/').pop()
+    let rawHtml = ''
+
+    try {
+      const { data: wiki } = await $axios.get(
+        `http://en.wikipedia.org/w/api.php?origin=*&action=parse&format=json&prop=text&section=0&page=${encodeURIComponent(
+          wikiPage
+        )}`
+      )
+
+      rawHtml = wiki.parse.text['*']
+    } catch (error) {
+      console.log(error)
+    }
+
+    return { author, rawHtml }
+  },
+  methods: {
+    handleQuotesButtonClicked() {
+      // This is a non-crawlable url to prove that 'quotes/*' pages are generated with an async function in nuxt.config.js
+      this.$router.push({
+        path: `/quotes/${encodeURIComponent(this.author.name)}`,
+      })
+    },
+  },
+}
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
+  <main class="pa3">
     <h1>Nuxt demo</h1>
-    <nav>
+    <nav class="pa2">
       <NuxtLink to="/about">About</NuxtLink>
-      <NuxtLink to="/quotes/Edsger%20W.%20Dijkstra"
-        >Quotes from Dijkstra</NuxtLink
-      >
+      <h2>Authors</h2>
+      <ul class="list pl0 center">
+        <li
+          v-for="authorLink in authorLinks"
+          :key="authorLink.url"
+          class="lh-copy pv3 ba bl-0 bt-0 br-0 b--dotted b--black-30"
+        >
+          <NuxtLink :to="authorLink.url">{{ authorLink.text }}</NuxtLink>
+        </li>
+      </ul>
     </nav>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -15,5 +22,17 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  async asyncData({ $axios }) {
+    const { data: authors = {} } = await $axios.get(
+      'https://programming-quotes-api.herokuapp.com/Authors/'
+    )
+
+    const authorLinks = Object.keys(authors).map((author) => ({
+      text: author,
+      url: `/authors/${encodeURIComponent(author)}`,
+    }))
+
+    return { authorLinks }
+  },
 })
 </script>
